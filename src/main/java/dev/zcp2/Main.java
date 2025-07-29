3404e3a2a840e63eca4988fbeead81f982c20ef6
Litezero <Litezero261@outlook.com>
2025-07-29 13:15:22 +0800

    从master分支同步

---
+++ src/main/java/dev/zcp2/Main.java
@@ -0,0 +1,29 @@
+package dev.zcp2;
+
+import dev.zcp2.config.WrappedConfig;
+import dev.zcp2.socket.WrappedWebSocketServer;
+import lombok.Getter;
+
+import java.io.File;
+import java.util.Objects;
+
+public final class Main {
+    @Getter
+    private static final WrappedConfig config = new WrappedConfig("config", true);
+    @Getter
+    private static File serverPath;
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void main(String[] args) {
+        config.load();
+        serverPath = new File(Main.getConfig().getJson().get("ServerPath").getAsString());
+        if(serverPath.listFiles() == null)
+            serverPath.mkdirs();
+        if(Objects.requireNonNull(serverPath.listFiles()).length == 0) {
+            // TODO:初始化(?)
+            WrappedWebSocketServer server = new WrappedWebSocketServer();
+            server.start(10086);
+            System.out.println("started");
+        }
+    }
+}
